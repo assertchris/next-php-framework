6072060f67a1d275cda59deccf2c20ef0234db08
Christopher Pitt <cgpitt@gmail.com>
2020-11-24 05:06:57 +0200

    Removed mixed type hints

--- source/Cache/Proxy.php
+++ source/Cache/Proxy.php
@@ -13,12 +13,12 @@ class Proxy
         static::$connection = new \Symfony\Component\Cache\Adapter\FilesystemAdapter();
     }
 
-    public function put(string $key, mixed $value, int $seconds = null)
+    public function put(string $key, $value, int $seconds = null)
     {
         return $this->store($key, $value, $seconds, true);
     }
 
-    private function store(string $key, mixed $value, int $seconds = null, bool $shouldOverride = true)
+    private function store(string $key, $value, int $seconds = null, bool $shouldOverride = true)
     {
         $item = static::$connection->getItem($key);
 
@@ -35,7 +35,7 @@ class Proxy
         return $this;
     }
 
-    public function add(string $key, mixed $value, int $seconds = null)
+    public function add(string $key, $value, int $seconds = null)
     {
         return $this->store($key, $value, $seconds, false);
     }
@@ -46,7 +46,7 @@ class Proxy
         return $item->isHit();
     }
 
-    public function get(string $key, mixed $default = null): mixed
+    public function get(string $key, $default = null)
     {
         $item = static::$connection->getItem($key);
 
@@ -61,7 +61,7 @@ class Proxy
         return $default;
     }
 
-    public function remember(string $key, \Closure $factory, int $seconds = null): mixed
+    public function remember(string $key, \Closure $factory, int $seconds = null)
     {
         $this->store($key, $value = $factory(), $seconds);
         return $value;
